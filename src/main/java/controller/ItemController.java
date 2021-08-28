929a94e104b6d7b7bd328ed0a4e6ab95618f30d7
Anete <ancss@inbox.lv>
2021-08-28 13:00:29 +0300

    Changes 3

--- src/main/java/controller/ItemController.java
+++ src/main/java/controller/ItemController.java
@@ -1,4 +1,29 @@
 package controller;
 
-public class ItemController {
-}
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class ItemController extends ViewController {
+
+    public TextField soldItemField;
+
+    public void handleExit(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "exit");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    public void handleBack(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "back");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+    }
+
