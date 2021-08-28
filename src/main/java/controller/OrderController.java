5f4c4753ba153cecea16a83dda471da18b027ba2
Madara <madara.rozkalne@gmail.com>
2021-08-28 13:35:29 +0300

    DBhandler

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -8,6 +8,16 @@ public class OrderController extends ViewController {
     public TextField ItemNameField;
     public TextField CustomerNameField;
     public TextField DeliveryMethodField;
+    public TextField ProductTypeField;
+    public TextField PriceField;
+    public TextField CountField;
+    public TextField GenderField;
+    public TextField ProduceTypeField;
+    public TextField SizeField;
+    public TextField ColorField;
+    public TextField TypeNameField;
+    public TextField CustomerEmailField;
+    public TextField CustomerPhoneField;
 
     public void handleAddOrder(ActionEvent actionEvent) {
     }
--- src/main/java/repository/DBHandler.java
+++ src/main/java/repository/DBHandler.java
@@ -1,4 +1,65 @@
 package repository;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import java.sql.*;
+
 public class DBHandler {
+
+        private static String pass;
+        private static String user;
+        private static String connectionUrl;
+        private static Connection connection;
+
+
+        DBHandler(){
+            getSetDatabaseInfo();
+        }
+
+        private static void getSetDatabaseInfo() {
+            PropertiesConfiguration databaseProperties = new PropertiesConfiguration();
+            try {
+                databaseProperties.load("database.properties");
+            } catch (ConfigurationException e) {
+                e.printStackTrace();
+            }
+            String host = databaseProperties.getString("database.host");
+            String port = databaseProperties.getString("database.port");
+            String dbName = databaseProperties.getString("database.name");
+
+
+            pass = databaseProperties.getString("database.password");
+            user = databaseProperties.getString("database.user");
+            connectionUrl = host + ":" + port + "/" + dbName + "?serverTimezone=GMT%2B3";
+        }
+
+        public static Connection getConnection() {
+            try {
+
+                if(connection == null || connection.isClosed()) {
+                    getSetDatabaseInfo();
+                    connection = DriverManager.getConnection(connectionUrl, user, pass);
+                }
+            } catch (SQLException ex) {
+                System.out.println("Unable to connect to database");
+                ex.printStackTrace();
+            }
+            return connection;
+
+
+        }
+
+
+        public static void close(ResultSet result, PreparedStatement preparedStatement, Connection connection) {
+            try {
+                result.close();
+                preparedStatement.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
 }
