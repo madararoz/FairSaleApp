149eceac52df142f21deef91b73d820aa6316ec3
Madara <madara.rozkalne@gmail.com>
2021-08-28 13:01:51 +0300

    add order view and OrderController

---
+++ src/main/java/controller/OrderController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class OrderController extends ViewController {
+
+    public TextField ItemNameField;
+    public TextField CustomerNameField;
+    public TextField DeliveryMethodField;
+
+    public void handleAddOrder(ActionEvent actionEvent) {
+    }
+
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    public void handleExit(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/controller/StartController.java
+++ src/main/java/controller/StartController.java
@@ -1,12 +1,25 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
-public class StartController {
+import java.io.IOException;
+
+public class StartController extends ViewController {
     
     public void registerSoldItem(ActionEvent actionEvent) {
+            try {
+                changeScene(actionEvent, "soldItems");
+            } catch (IOException e) {
+                showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+            }
     }
 
     public void registerOrderedItem(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "order");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
