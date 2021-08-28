11c8ee11965a48182d0f7f4936fb57e557e9e8cf
Anete <ancss@inbox.lv>
2021-08-28 13:33:56 +0300

    Changes 4

--- src/main/java/controller/ItemController.java
+++ src/main/java/controller/ItemController.java
@@ -9,6 +9,15 @@ import java.io.IOException;
 public class ItemController extends ViewController {
 
     public TextField soldItemField;
+    public TextField productTypeField;
+    public TextField priceField;
+    public TextField countField;
+    public TextField genderField;
+    public TextField produceField;
+    public TextField sizeField;
+    public TextField colourField;
+    public TextField typeNameField;
+    public TextField sellerField;
 
     public void handleExit(ActionEvent actionEvent) {
         try {
@@ -25,5 +34,13 @@ public class ItemController extends ViewController {
             showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
+
+    public void handleAdd(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "add");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
+}
 
