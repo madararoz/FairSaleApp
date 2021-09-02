fd1a96e90a241670a56568075ec80b8a30962631
santavizma <santa@vivalatour.lv>
2021-09-02 20:30:40 +0300

    Scenes/Buttons Day reports

--- src/main/java/controller/StartController.java
+++ src/main/java/controller/StartController.java
@@ -22,4 +22,27 @@ public class StartController extends ViewController {
             showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
+
+    public void viewSoldItemDayReport(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "daySoldReport");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    public void viewOrderItemDayReport(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "dayOrderReport");
+        } catch (IOException e) {
+            showAlert("Problem loading scene", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+    public void handleBack(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "start");
+        } catch (IOException ex) {
+            showAlert("Back failed", ex.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
 }
