149da128e8b19d0d92d30e32855463bb5fc82046
santavizma <santa@vivalatour.lv>
2021-09-02 21:46:44 +0300

    Scenes/Buttons of Day reports 3rd

---
+++ src/main/java/repository/Queries.java
@@ -0,0 +1,8 @@
+package repository;
+
+public class Queries {
+
+    public static final String SELECT_ITEMS = "SELECT * FROM items;";
+
+
+}
---
+++ src/main/java/service/ReportDBService.java
@@ -0,0 +1,48 @@
+package service;
+import controller.ViewController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import model.Item;
+import repository.DBHandler;
+import repository.Queries;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class ReportDBService extends ViewController {
+    private Connection connection = DBHandler.getConnection();
+
+    public ArrayList<Item> getSoldItems(ActionEvent actionEvent) throws SQLException {
+
+        ArrayList<Item> soldItems = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement(Queries.SELECT_ITEMS);
+        ResultSet result = statement.executeQuery();
+
+//            while (result.next()) {
+//                Item item = new Item(
+//                        result.getInt("id"),
+//                        new Item(result.getInt("product_type"), result.getString("owner_name")),
+//
+//                );
+//                soldItems.add(item);
+//            }
+        return soldItems;
+    }
+
+    public void getOrderedItems(ActionEvent actionEvent) {
+    }
+
+    public void handleBack(ActionEvent actionEvent) {
+        try {
+            changeScene(actionEvent, "start");
+        } catch (IOException ex) {
+            showAlert("Back failed", ex.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+}
